a008a3c4f975f89ce0bee0cb3aacc1f88b38e72f
Nadun Viduranga <nadunviduranga54321@gmail.com>
2022-07-19 00:40:52 +0530

    Tutorial-03.

--- src/main/scala/FunctionalPrograming/Tutorial03/Quction5.scala
+++ src/main/scala/FunctionalPrograming/Tutorial03/Quction5.scala
@@ -5,7 +5,7 @@ object Quection05 extends App{
   //find easy run
   def easy(k:Int) = k*8;
 
-  //find the Tempo
+  //find the Tempo..
   def Tempo(k:Int) = k*7;
 
   val totalT = easy(2) + Tempo(3) + easy(2);
